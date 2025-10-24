c72422a4b59d35886ba55c28ec307de0ff3724b9
pavansai prathi <pavansainaidu37@gmail.com>
2025-10-24 18:22:47 +0530

    services in home page

--- src/components/HomeOne/Footer2.jsx
+++ src/components/HomeOne/Footer2.jsx
@@ -4,14 +4,6 @@ function Footer() {
   return (
     <footer className="cracking-footerr">
       <div className="footer-contentt">
-        <div className="contact-line">
-    <p>
-      Let’s Collaborate{" "}
-      <a href="/contact" className="contact-btn1">
-        Contact Us
-      </a>
-    </p>
-  </div>
         <h3>START YOUR JOURNEY WITH US</h3>
         <p>
           info@pranisha.com / +91 99633 26393
